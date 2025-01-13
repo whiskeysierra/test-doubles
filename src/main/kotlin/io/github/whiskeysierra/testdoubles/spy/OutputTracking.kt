a6ce0c2ac5304dfb15282fd0e7465a6024cf960c
Willi Schönborn <w.schoenborn@gmail.com>
2025-01-13 09:12:23 +0100

    Renamed Active to Subscription

--- src/main/kotlin/io/github/whiskeysierra/testdoubles/spy/OutputTracking.kt
+++ src/main/kotlin/io/github/whiskeysierra/testdoubles/spy/OutputTracking.kt
@@ -6,20 +6,21 @@ import java.util.concurrent.CopyOnWriteArrayList
  * @see <a href="https://www.jamesshore.com/v2/projects/testing-without-mocks/testing-without-mocks#output-tracking">Testing without mocks: Output Tracking</a>
  */
 class OutputTracking<Output> {
-    private inner class Active(
+
+    private inner class Subscription(
         private val output: MutableList<Output> = ArrayList(),
-    ) : Tracker<Output>,
-        MutableList<Output> by output {
+    ) : Tracker<Output>, MutableList<Output> by output {
+
         override fun clear() = output.clear()
 
         override fun close() {
-            trackers.remove(this)
+            subscriptions.remove(this)
         }
     }
 
-    private val trackers = CopyOnWriteArrayList<Active>()
+    private val subscriptions = CopyOnWriteArrayList<Subscription>()
 
-    fun emit(data: Output) = trackers.forEach { it.add(data) }
+    fun emit(data: Output) = subscriptions.forEach { it.add(data) }
 
-    fun track(): Tracker<Output> = Active().also(trackers::add)
+    fun track(): Tracker<Output> = Subscription().also(subscriptions::add)
 }
