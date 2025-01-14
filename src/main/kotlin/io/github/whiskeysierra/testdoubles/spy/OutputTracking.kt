9216e07d15ecf043bb50650de93a17604f73c76c
Willi Schönborn <w.schoenborn@gmail.com>
2025-01-14 14:29:02 +0100

    Fixed formatting issues

--- src/main/kotlin/io/github/whiskeysierra/testdoubles/spy/OutputTracking.kt
+++ src/main/kotlin/io/github/whiskeysierra/testdoubles/spy/OutputTracking.kt
@@ -6,13 +6,11 @@ import java.util.concurrent.CopyOnWriteArrayList
  * @see <a href="https://www.jamesshore.com/v2/projects/testing-without-mocks/testing-without-mocks#output-tracking">Testing without mocks: Output Tracking</a>
  */
 class OutputTracking<Output> {
-
     private class Subscription<Output>(
         private val tracking: OutputTracking<Output>,
         private val outputs: MutableList<Output> = ArrayList(),
     ) : Tracker<Output>,
         List<Output> by outputs {
-
         fun add(output: Output) {
             outputs.add(output)
         }
@@ -26,12 +24,9 @@ class OutputTracking<Output> {
 
     private val subscriptions = CopyOnWriteArrayList<Subscription<Output>>()
 
-    fun add(output: Output): Unit =
-        subscriptions.forEach { it.add(output) }
+    fun add(output: Output): Unit = subscriptions.forEach { it.add(output) }
 
-    fun track(): Tracker<Output> =
-        Subscription(this).also(subscriptions::add)
+    fun track(): Tracker<Output> = Subscription(this).also(subscriptions::add)
 
-    internal fun remove(subscription: Tracker<Output>) =
-        subscriptions.remove(subscription)
+    internal fun remove(subscription: Tracker<Output>) = subscriptions.remove(subscription)
 }
