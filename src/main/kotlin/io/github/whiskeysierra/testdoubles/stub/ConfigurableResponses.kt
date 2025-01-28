8d2c48eb603b07f89ed959fc54278406c83f0529
Willi Schönborn <w.schoenborn@gmail.com>
2025-01-28 05:17:37 +0100

    Allow multiple values for always

--- src/main/kotlin/io/github/whiskeysierra/testdoubles/stub/ConfigurableResponses.kt
+++ src/main/kotlin/io/github/whiskeysierra/testdoubles/stub/ConfigurableResponses.kt
@@ -4,7 +4,7 @@ package io.github.whiskeysierra.testdoubles.stub
  * @see <a href="https://www.jamesshore.com/v2/projects/testing-without-mocks/testing-without-mocks#configurable-responses">Testing without Mocks: Configurable Responses</a>
  */
 object ConfigurableResponses {
-    fun <T> always(value: T) = generateSequence { value }.iterator()
+    fun <T> always(vararg values: T) = sequence { while (true) yieldAll(values.iterator()) }.iterator()
 
     fun <T> once(vararg values: T) = values.iterator()
 }
--- src/test/kotlin/io/github/whiskeysierra/testdoubles/stub/ConfigurableResponsesTest.kt
+++ src/test/kotlin/io/github/whiskeysierra/testdoubles/stub/ConfigurableResponsesTest.kt
@@ -4,52 +4,94 @@ import io.github.whiskeysierra.testdoubles.stub.ConfigurableResponses.always
 import io.github.whiskeysierra.testdoubles.stub.ConfigurableResponses.once
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class ConfigurableResponsesTest {
-    @Test
-    fun `returns configured response forever`() {
-        val values = always("value")
 
-        assertThat(values.next()).isEqualTo("value")
-        assertThat(values.next()).isEqualTo("value")
-        assertThat(values.next()).isEqualTo("value")
+    @DisplayName("always")
+    @Nested
+    inner class Always {
+
+        @Test
+        fun `throws if no response was configured`() {
+            val values = always("value")
+
+            assertThat(values.next()).isEqualTo("value")
+            assertThat(values.next()).isEqualTo("value")
+            assertThat(values.next()).isEqualTo("value")
+        }
+
+        @Test
+        fun `returns configured response forever`() {
+            val values = always("value")
+
+            assertThat(values.next()).isEqualTo("value")
+            assertThat(values.next()).isEqualTo("value")
+            assertThat(values.next()).isEqualTo("value")
+        }
+
+        @Test
+        fun `returns configured responses forever`() {
+            val values = always("1st", "2nd", "3rd")
+
+            repeat(3) { values.next() }
+
+            assertThat(values.next()).isEqualTo("1st")
+            assertThat(values.next()).isEqualTo("2nd")
+            assertThat(values.next()).isEqualTo("3rd")
+        }
+
+        @Test
+        fun `supports responses of different types`() {
+            val values = always(1, 2.0, "three")
+
+            assertThat(values.next()).isEqualTo(1)
+            assertThat(values.next()).isEqualTo(2.0)
+            assertThat(values.next()).isEqualTo("three")
+        }
     }
 
-    @Test
-    fun `throws if no response was configured`() {
-        val values = once<String>()
+    @DisplayName("once")
+    @Nested
+    inner class Once {
 
-        assertThatThrownBy { values.next() }
-            .isInstanceOf(NoSuchElementException::class.java)
-    }
+        @Test
+        fun `throws if no response was configured`() {
+            val values = once<String>()
 
-    @Test
-    fun `returns configured response`() {
-        val values = once("1st", "2nd", "3rd")
+            assertThatThrownBy { values.next() }
+                .isInstanceOf(NoSuchElementException::class.java)
+        }
 
-        assertThat(values.next()).isEqualTo("1st")
-        assertThat(values.next()).isEqualTo("2nd")
-        assertThat(values.next()).isEqualTo("3rd")
-    }
+        @Test
+        fun `returns configured response`() {
+            val values = once("1st", "2nd", "3rd")
 
-    @Test
-    fun `throws if no more configured responses`() {
-        val values = once("1st", "2nd", "3rd")
+            assertThat(values.next()).isEqualTo("1st")
+            assertThat(values.next()).isEqualTo("2nd")
+            assertThat(values.next()).isEqualTo("3rd")
+        }
 
-        repeat(3) { values.next() }
+        @Test
+        fun `throws if no more configured responses`() {
+            val values = once("1st", "2nd", "3rd")
 
-        assertThat(values).isExhausted
-        assertThatThrownBy { values.next() }
-            .isInstanceOf(NoSuchElementException::class.java)
-    }
+            repeat(3) { values.next() }
 
-    @Test
-    fun `supports responses of different types`() {
-        val values = once(1, 2.0, "three")
+            assertThat(values).isExhausted
+            assertThatThrownBy { values.next() }
+                .isInstanceOf(NoSuchElementException::class.java)
+        }
 
-        assertThat(values.next()).isEqualTo(1)
-        assertThat(values.next()).isEqualTo(2.0)
-        assertThat(values.next()).isEqualTo("three")
+        @Test
+        fun `supports responses of different types`() {
+            val values = once(1, 2.0, "three")
+
+            assertThat(values.next()).isEqualTo(1)
+            assertThat(values.next()).isEqualTo(2.0)
+            assertThat(values.next()).isEqualTo("three")
+        }
     }
 }
